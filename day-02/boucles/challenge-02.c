91c162f2c46465c67a464e1557b050747cbc0dc6
ayoub-ahnaou <ayoubahnaou@gmail.com>
2024-09-19 17:17:02 +0100

    challenge 2 added

---
+++ day-02/boucles/challenge-02.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    // pyramide d'etoile
+    
+    int n;
+    printf("donner un nombre n: ");
+    scanf("%d", &n);
+
+    for(int i=0; i<n; i++){
+        for(int j=i+1; j<n; j++){
+            printf("  ");
+        }
+        for(int k=0; k<(i+1)*2-1; k++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+
+    return 0;
+}
