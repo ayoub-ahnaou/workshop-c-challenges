41c3177d7d6d354d5a11913e80fdefa567b8f968
ayoub-ahnaou <ayoubahnaou@gmail.com>
2024-09-19 17:40:08 +0100

    challenge 1 added

---
+++ day-03/strings/challenge-01.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+    char chaine[25];
+    printf("donner une chaine de caractere: ");
+    scanf("%s", chaine);
+
+    printf("%s", chaine);
+    
+    printf("\n");
+    return 0;
+}
